0960fe7ba5c0e48a413bd0c95bba32117af1ad7c
bluebean <1401597377@qq.com>
2024-04-02 18:56:06 +0800

    new

---
+++ BizerCurve3D/Assets/Scripts/BizerCurveRuntimeEditor.cs
@@ -0,0 +1,77 @@
+﻿#if UNITY_EDITOR
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(BizerCurve))]
+public class BizerCurveRuntimeEditor : MonoBehaviour
+{
+    public GameObject m_player;
+    public List<Vector3> m_pathPoints;
+
+    private BizerCurve m_curve = null;
+
+    private void Awake()
+    {
+        m_curve = this.GetComponent<BizerCurve>();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    IEnumerator Move()
+    {
+        if (m_pathPoints.Count == 0) yield break;
+        int item = 1;
+        while (true)
+        {
+            m_player.transform.LookAt(m_pathPoints[item]);
+            m_player.transform.position = Vector3.Lerp(m_pathPoints[item - 1], m_pathPoints[item], 1f);
+            item++;
+            if (item >= m_pathPoints.Count)
+            {
+                item = 1;
+                yield break;
+            }
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftControl) && (Input.GetMouseButtonUp(0) || Input.GetMouseButtonUp(1)))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (Input.GetMouseButtonUp(0) && hit.collider.tag.Equals("Terrain"))
+                {
+                    Vector3 pointPos = new Vector3(hit.point.x, m_player.transform.position.y, hit.point.z);
+                    m_curve.AddPoint(pointPos);
+                }
+                else if (Input.GetMouseButtonUp(1) && hit.collider.tag.Equals("AnchorPoint"))
+                {
+                    m_curve.DeletePoint(hit.collider.gameObject);
+                }
+            }
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+            m_pathPoints = m_curve.HiddenLine(false);
+        else if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            m_curve.HiddenLine(true);
+            m_pathPoints.Clear();
+        }
+        if (Input.GetKeyUp(KeyCode.B))
+        {
+            StartCoroutine(Move());
+        }
+    }
+}
+
+#endif
--- BizerCurve3D/Assets/Scripts/CurvePointControl.cs
+++ BizerCurve3D/Assets/Scripts/CurvePointControl.cs
@@ -2,105 +2,116 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace DM.Editor.View
+[ExecuteInEditMode]
+public class CurvePointControl : MonoBehaviour
 {
-    public class CurvePointControl : MonoBehaviour
+    [Header("锁定X轴")]
+    public bool m_isLockX = false;
+    [Header("锁定Y轴")]
+    public bool m_isLockY = true;
+    [Header("锁定Z轴")]
+    public bool m_isLockZ = false;
+
+    [HideInInspector]
+    public GameObject m_controlObject;
+    [HideInInspector]
+    public GameObject m_controlObject2;
+
+    private Vector3 m_offsetPos1 = Vector3.zero;
+    private Vector3 m_offsetPos2 = Vector3.zero;
+    private LineRenderer m_lineRenderer;
+
+    private BizerCurve m_curve = null;
+
+    private void Awake()
     {
-        [Header("锁定X轴")]
-        public bool m_isLockX = false;
-        [Header("锁定Y轴")]
-        public bool m_isLockY = true;
-        [Header("锁定Z轴")]
-        public bool m_isLockZ = false;
-       
-        [HideInInspector]
-        public GameObject m_controlObject;
-        [HideInInspector]
-        public GameObject m_controlObject2;
-
-        private Vector3 offsetPos1 = Vector3.zero;
-        private Vector3 offsetPos2 = Vector3.zero;
-        private LineRenderer lineRenderer;
-
-        void Start()
-        {
-            if (gameObject.tag.Equals("AnchorPoint") && !lineRenderer)
-                lineRenderer = gameObject.AddComponent<LineRenderer>();
-            if (lineRenderer)
-            {
-                lineRenderer.sortingOrder = 1;
-                lineRenderer.material = new Material(Shader.Find("Legacy Shaders/Particles/Alpha Blended"));
-                lineRenderer.startColor = lineRenderer.endColor = Color.yellow;
-                lineRenderer.widthMultiplier = 0.03f;
-                lineRenderer.positionCount = 0;
-            }
-        }
-
-        void OnMouseDown()
-        {
-            if (!gameObject.tag.Equals("AnchorPoint")) return;
-            OffsetPos();
-        }
-
-        public List<Vector3> OffsetPos()
-        {
-            List<Vector3> offsetPosList = new List<Vector3>();
-            if (m_controlObject)
-                offsetPos1 = m_controlObject.transform.position - transform.position;
-            if (m_controlObject2)
-                offsetPos2 = m_controlObject2.transform.position - transform.position;
-            offsetPosList.Add(offsetPos1);
-            offsetPosList.Add(offsetPos2);
-
-            return offsetPosList;
-        }
-
-        void OnMouseDrag()
-        {
-            //if (gameObject.tag.Equals("AnchorPoint")) return;
-            Vector3 pos0 = Camera.main.WorldToScreenPoint(transform.position);
-            Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, pos0.z);
-            Vector3 mousePosInWorld= Camera.main.ScreenToWorldPoint(mousePos);
-            Vector3 thisPos = mousePosInWorld;
-            if (m_isLockX)
-                thisPos.x = transform.position.x;
-            if (m_isLockY)
-                thisPos.y = transform.position.y;
-            if (m_isLockZ)
-                thisPos.z = transform.position.z;
-            transform.position = thisPos;
-            DMDrawCurve.Instance.UpdateLine(gameObject, offsetPos1, offsetPos2);   
-        }      
-
-        private void DrawControlLine()
-        {
-            if (!gameObject.tag.Equals("AnchorPoint") || (!m_controlObject && !m_controlObject2)) return;
-            if (lineRenderer)
-            {
-                lineRenderer.positionCount = (m_controlObject && m_controlObject2) ? 3 : 2;
-                if (m_controlObject && !m_controlObject2)
-                {
-                    lineRenderer.SetPosition(0, m_controlObject.transform.position);
-                    lineRenderer.SetPosition(1, transform.position);
-                }
-                if (m_controlObject2 && !m_controlObject)
-                {
-                    lineRenderer.SetPosition(0, transform.position);
-                    lineRenderer.SetPosition(1, m_controlObject2.transform.position);
-                }
-                if (m_controlObject && m_controlObject2)
-                {
-                    lineRenderer.SetPosition(0, m_controlObject.transform.position);
-                    lineRenderer.SetPosition(1, transform.position);
-                    lineRenderer.SetPosition(2, m_controlObject2.transform.position);
-                }
-            }
-        }
-
-        void Update()
-        {
-            DrawControlLine();
-        }
-
+        m_curve = GetComponentInParent<BizerCurve>();
     }
+
+    public LineRenderer LineRenderer
+    {
+        get
+        {
+            if (gameObject.tag.Equals("AnchorPoint") && !m_lineRenderer)
+            {
+                m_lineRenderer = gameObject.AddComponent<LineRenderer>();
+                if (m_lineRenderer)
+                {
+                    m_lineRenderer.sortingOrder = 1;
+                    m_lineRenderer.material = new Material(Shader.Find("Legacy Shaders/Particles/Alpha Blended"));
+                    m_lineRenderer.startColor = m_lineRenderer.endColor = Color.yellow;
+                    m_lineRenderer.widthMultiplier = 0.03f;
+                    m_lineRenderer.positionCount = 0;
+                }
+            }
+            return m_lineRenderer;
+        }
+    }
+
+    void OnMouseDown()
+    {
+        //Debug.Log("CurvePointControl:OnMouseDown");
+        if (!gameObject.tag.Equals("AnchorPoint")) return;
+        OffsetPos();
+    }
+
+    public List<Vector3> OffsetPos()
+    {
+        List<Vector3> offsetPosList = new List<Vector3>();
+        if (m_controlObject)
+            m_offsetPos1 = m_controlObject.transform.position - transform.position;
+        if (m_controlObject2)
+            m_offsetPos2 = m_controlObject2.transform.position - transform.position;
+        offsetPosList.Add(m_offsetPos1);
+        offsetPosList.Add(m_offsetPos2);
+
+        return offsetPosList;
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 pos0 = Camera.main.WorldToScreenPoint(transform.position);
+        Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, pos0.z);
+        Vector3 mousePosInWorld = Camera.main.ScreenToWorldPoint(mousePos);
+        Vector3 thisPos = mousePosInWorld;
+        if (m_isLockX)
+            thisPos.x = transform.position.x;
+        if (m_isLockY)
+            thisPos.y = transform.position.y;
+        if (m_isLockZ)
+            thisPos.z = transform.position.z;
+        transform.position = thisPos;
+        m_curve.UpdateLine(gameObject, m_offsetPos1, m_offsetPos2);
+    }
+
+    private void DrawControlLine()
+    {
+        if (!gameObject.tag.Equals("AnchorPoint") || (!m_controlObject && !m_controlObject2)) return;
+        if (LineRenderer)
+        {
+            LineRenderer.positionCount = (m_controlObject && m_controlObject2) ? 3 : 2;
+            if (m_controlObject && !m_controlObject2)
+            {
+                LineRenderer.SetPosition(0, m_controlObject.transform.position);
+                LineRenderer.SetPosition(1, transform.position);
+            }
+            if (m_controlObject2 && !m_controlObject)
+            {
+                LineRenderer.SetPosition(0, transform.position);
+                LineRenderer.SetPosition(1, m_controlObject2.transform.position);
+            }
+            if (m_controlObject && m_controlObject2)
+            {
+                LineRenderer.SetPosition(0, m_controlObject.transform.position);
+                LineRenderer.SetPosition(1, transform.position);
+                LineRenderer.SetPosition(2, m_controlObject2.transform.position);
+            }
+        }
+    }
+
+    void Update()
+    {
+        DrawControlLine();
+    }
+
 }
